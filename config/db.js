2f7d0df62df4475d590942f6dc85773af2b8518e
amir5124 <bocahangon64@gmail.com>
2026-02-01 09:57:10 +0700

    add port

--- config/db.js
+++ config/db.js
@@ -6,6 +6,7 @@ const pool = mysql.createPool({
     user: process.env.DB_USER || 'mysql',
     password: process.env.DB_PASS || 'HBUduUGKu7FfNtySo8BVr151PmyP5J5opS0J8UW9egKGkuTe4nQeoTLJadD1QXFm',
     database: process.env.DB_NAME || 'tangerang_mandiri',
+    port: 32771,
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
