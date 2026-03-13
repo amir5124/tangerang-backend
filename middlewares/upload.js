da1806abe9b345ed5c5bdd64d4bf8dfd1de18395
unknown <bocahangon64@gmail.com>
2026-03-14 06:43:50 +0700

    p

--- middlewares/upload.js
+++ middlewares/upload.js
@@ -1,14 +1,25 @@
 const multer = require('multer');
 const path = require('path');
+const fs = require('fs');
+
+// Memastikan folder ada
+const uploadDir = 'uploads/services';
+if (!fs.existsSync(uploadDir)) {
+    fs.mkdirSync(uploadDir, { recursive: true });
+}
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, 'uploads/assets/'); // Pastikan folder ini ada
+    cb(null, uploadDir); 
   },
   filename: (req, file, cb) => {
-    cb(null, `asset-${Date.now()}${path.extname(file.originalname)}`);
+    cb(null, Date.now() + path.extname(file.originalname));
   }
 });
 
-const upload = multer({ storage });
+const upload = multer({ 
+  storage: storage,
+  limits: { fileSize: 5 * 1024 * 1024 }
+});
+
 module.exports = upload;
