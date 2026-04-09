51e187cf43995b07d0175e27434d84aec72bb093
unknown <bocahangon64@gmail.com>
2026-04-10 05:14:02 +0700

    wall

--- routes/balanceRoutes.js
+++ routes/balanceRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const { getWalletBalance } = require('../controllers/balanceController');
-const { authenticateToken } = require('../middleware/authMiddleware'); // Menggunakan middleware kamu
+const { authenticateToken } = require('../middlewares/authMiddleware');
 
 // Endpoint: GET /api/balance
 // Siapapun yang login bisa akses wallet miliknya sendiri
