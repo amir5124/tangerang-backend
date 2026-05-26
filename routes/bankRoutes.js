6e692426dbf54142ab9b07e04eecee72803732f1
amir5124 <bocahangon64@gmail.com>
2026-05-26 18:50:57 +0700

    lok

--- routes/bankRoutes.js
+++ routes/bankRoutes.js
@@ -5,12 +5,12 @@ const bankController = require('../controllers/bankController');
 
 
 // Route untuk bank accounts
-router.get('/banks/list', bankController.getBankList);
-router.post('/banks/accounts', bankController.addBankAccount);
-router.get('/banks/accounts', bankController.getBankAccounts);
-router.get('/banks/accounts/active', bankController.getActiveBankAccount);
-router.put('/banks/accounts/:id', bankController.updateBankAccount);
-router.delete('/banks/accounts/:id', bankController.deleteBankAccount);
-router.patch('/banks/accounts/:id/active', bankController.setActiveBankAccount);
+router.get('/list', bankController.getBankList);
+router.post('/accounts', bankController.addBankAccount);
+router.get('/accounts', bankController.getBankAccounts);
+router.get('/accounts/active', bankController.getActiveBankAccount);
+router.put('/accounts/:id', bankController.updateBankAccount);
+router.delete('/accounts/:id', bankController.deleteBankAccount);
+router.patch('/accounts/:id/active', bankController.setActiveBankAccount);
 
 module.exports = router;
